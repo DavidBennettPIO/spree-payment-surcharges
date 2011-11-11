25b2aaead9de00d86259af28cac01eec6991b18a
David Bennett <davidbennett@bravevision.com>
2011-11-11 15:07:40 +1030

    cleanup calc. and support muli-payment.

--- app/models/calculator/flat_percent_payable.rb
+++ app/models/calculator/flat_percent_payable.rb
@@ -9,21 +9,19 @@ class Calculator::FlatPercentPayable < Calculator
     super
   end
 
-  def compute(object)
-    return 0 unless object.present?
-    if object.class.name.split('::').last.downcase == 'order'
-      order = object
-    elsif object.order.present?
-      order = object.order
-    else
-      return 0
+  def compute(order)
+    return 0 unless order.present?
+    
+    unless order.is_a? Order
+      return 0 unless order.order.is_a? Order
+      order = order.order
     end
+
     return 0 unless order.item_total.present?
 
-    item_total = order.item_total
     adjustment_total = order.adjustments.without_payment_surcharge.map(&:amount).sum
     
-    payable_total = item_total + adjustment_total
+    payable_total = order.item_total + adjustment_total - order.payment_total
 
     payable_total * (self.preferred_flat_percent_payable / 100.0)
   end
